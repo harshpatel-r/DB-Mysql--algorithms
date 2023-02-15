b9a2d5369a8fd5e8e37b05515583f19577b50ebb
Harsh Patel <harshpatel23900@gmail.com>
2023-02-15 17:47:01 +0530

    HackerRank | MySQL | Easy | Japanese Cities' Attributes

---
+++ HackerRank/JapaneseCities'Attributes.sql
@@ -0,0 +1,10 @@
+
+-- # Japanese Cities' Attributes
+
+/*
+Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN. 
+*/
+
+-- SOLUTION
+
+SELECT * FROM CITY WHERE COUNTRYCODE = 'JPN';
