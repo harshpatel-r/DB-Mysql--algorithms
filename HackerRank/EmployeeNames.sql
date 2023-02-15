cf3f9aff24540e608d8dbdfe4b024a9a4cc0eb34
Harsh Patel <harshpatel23900@gmail.com>
2023-02-15 18:25:05 +0530

    HackerRank | MySQL | Easy | Employee Names

---
+++ HackerRank/EmployeeNames.sql
@@ -0,0 +1,11 @@
+
+--# Employee Names
+
+/*
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee 
+table in alphabetical order.
+*/
+
+-- SOLUTION
+
+SELECT NAME FROM EMPLOYEE ORDER BY NAME;
