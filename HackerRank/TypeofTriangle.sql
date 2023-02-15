1ab0ebcc604699db85cea91e3a6dce7bca07d5e2
Harsh Patel <harshpatel23900@gmail.com>
2023-02-15 18:32:48 +0530

    HackerRank | MySQL | Easy | Type of Triangle

---
+++ HackerRank/TypeofTriangle.sql
@@ -0,0 +1,22 @@
+
+--# Type of Triangle
+
+/*
+Write a query identifying the type of each record in the TRIANGLES table using its three side 
+lengths. Output one of the following statements for each record in the table:
+
+-> Equilateral: It's a triangle with 3 sides of equal length.
+-> Isosceles: It's a triangle with 2 sides of equal length.
+-> Scalene: It's a triangle with 3 sides of differing lengths.
+-> Not A Triangle: The given values of A, B, and C don't form a triangle.
+*/
+
+-- SOLUTION
+
+SELECT CASE 
+WHEN (A=B AND B=C) THEN "Equilateral"
+WHEN (A+B <=C OR B+C <=A OR C+A <=B) THEN "Not A Triangle"
+WHEN (A=B OR  B=C OR C=A) THEN "Isosceles"
+ELSE "Scalene"
+END 
+FROM TRIANGLES;
