db986a3cc4a5739a28e97b67fe160e913724160a
Harsh Patel <harshpatel23900@gmail.com>
2023-02-15 17:58:03 +0530

    HackerRank | MySQL | Easy | Weather Observation Station 3

---
+++ HackerRank/WeatherObservationStation3.sql
@@ -0,0 +1,11 @@
+
+--# Weather Observation Station 3
+
+/*
+Query a list of CITY names from STATION for cities that have an even ID number. Print the results in 
+any order, but exclude duplicates from the answer. 
+*/
+
+-- SOLUTION
+
+SELECT DISTINCT(CITY) FROM STATION WHERE ID%2 = 0;
