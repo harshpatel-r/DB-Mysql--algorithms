eab28b11bd2d26ed1b53feca5567b6718258321f
Harsh Patel <harshpatel23900@gmail.com>
2023-02-15 17:30:15 +0530

    HackerRank | MySQL | Revising the Select Query I

---
+++ HackerRank/RevisingtheSelectQueryI.sql
@@ -0,0 +1,14 @@
+
+-- //# Revising the Select Query I
+
+/*
+Query all columns for all American cities in the CITY table with populations larger than 100000. The 
+CountryCode for America is USA.
+
+The CITY table is described as follows: 
+*/
+
+
+-- SOLUTION
+
+select * from CITY where CountryCode = 'USA' and population > 100000 ;
